75805cd13dc4c91b3106656357aa5b49b10ae260
Vibin <vibinrs447@gmail.com>
2025-08-18 14:49:14 +0530

    First commit-commiting all files

---
+++ first.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main () {
+    printf("Name : Vibin\n");
+    printf("Home Name : Sujatha Bhavan\n");
+    printf("Address : 8/61,\n kaithakuzhi,\n kaliyakkavilai post,\n kanyakumari dist.\n");
+
+    return 0;
+}
