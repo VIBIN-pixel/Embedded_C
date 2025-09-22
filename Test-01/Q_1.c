9a7694fcf4f947c9007c413dc57fc8c27e9a459a
Vibin <vibinrs447@gmail.com>
2025-09-22 21:48:54 +0530

    Commiting all files

---
+++ Test-01/Q_1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int a[3]= {1,2,3};
+int a;
+
+int main () {
+    printf("Second largest element= %d",a[1];
+    printf("Size of integer= %d\n", sizeof(int));
+
+    return a(1);
+    
+}
---
+++ Test-01/Q_3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int i,mask,i=8;
+     
+
+     mask = 3<<i;
+     printf("1");
+     else
+     printf("0");
+     
+
+int main() {
+    int a;
+    printf("Enter a number: ");
+    scanf("%d", &a);
+    printBin (a);
+    return 0;
+}
